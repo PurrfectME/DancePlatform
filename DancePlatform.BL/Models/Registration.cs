f6f59b1243241db3b45fc07e56f47b8a761f4035
Дмитрий Ефремов <it.efr99@gmail.com>
2020-11-26 13:41:44 +0300

    add startup version

---
+++ DancePlatform.BL/Models/Registration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DancePlatform.BL.Models
+{
+    public class Registration
+    {
+    }
+}
---
+++ DancePlatform.BL/Models/Workshop.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DancePlatform.BL.Models
+{
+    public class Workshop
+    {
+
+    }
+}
